8ccb21f08584953e29cf92c7f999c444e38d3f06
Riley Smith <github@rileyy.dev>
2026-03-11 16:16:19 -0700

    feat: first pass at autograding flow

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,13 @@ import * as vscode from 'vscode';
 import { SidebarProvider } from './sidebarProvider';
 import { ApiService } from './services/apiService';
 import { TestingService } from './services/testingService';
+import { GitService } from './services/gitService';
 
 export function activate(context: vscode.ExtensionContext): void {
     const apiService = ApiService.getInstance(context, '');
     const testingService = new TestingService(context, apiService);
-    const sidebarProvider = new SidebarProvider(context, testingService);
+    const gitService = new GitService();
+    const sidebarProvider = new SidebarProvider(context, testingService, gitService);
 
     context.subscriptions.push(
         vscode.window.registerWebviewViewProvider('submittyWebview', sidebarProvider)
---
+++ src/typings/message.ts
@@ -0,0 +1,11 @@
+export const MessageCommand = {
+    FETCH_AND_DISPLAY_COURSES: 'fetchAndDisplayCourses',
+    GRADE: 'grade',
+    GRADE_STARTED: 'gradeStarted',
+    GRADE_COMPLETED: 'gradeCompleted',
+    GRADE_ERROR: 'gradeError',
+    GRADE_CANCELLED: 'gradeCancelled',
+    GRADE_PAUSED: 'gradePaused',
+    GRASE_RESUMED: 'gradeResumed',
+    GRADE_ABORTED: 'gradeAborted',
+} as const;
