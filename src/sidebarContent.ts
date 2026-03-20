0013edf26cd81c53d85b91c0963a603b2ac15226
Riley Smith <github@rileyy.dev>
2026-03-20 00:04:29 -0700

    chore: remove login html

--- src/sidebarContent.ts
+++ src/sidebarContent.ts
@@ -2,16 +2,6 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
 
-export function getLoginHtml(context: vscode.ExtensionContext): string {
-  const filePath = path.join(
-    context.extensionPath,
-    'src',
-    'sidebar',
-    'login.html'
-  );
-  return fs.readFileSync(filePath, 'utf8');
-}
-
 export function getClassesHtml(context: vscode.ExtensionContext): string {
   const filePath = path.join(
     context.extensionPath,
